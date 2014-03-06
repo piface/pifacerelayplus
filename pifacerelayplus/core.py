f08c03ca266d92ed0270dd23d741e85329a5e1e9
Thomas Preston <thomasmarkpreston@gmail.com>
2014-03-06 17:11:33 +0000

    added working stepper motor code, it is not very fast

--- pifacerelayplus/core.py
+++ pifacerelayplus/core.py
@@ -1,5 +1,6 @@
 import pifacecommon.interrupts
 import pifacecommon.mcp23s17
+import time
 
 
 # /dev/spidev<bus>.<chipselect>
@@ -8,20 +9,21 @@ DEFAULT_SPI_CHIP_SELECT = 0
 
 # Datasheet says coast is 0, 0 and bake is 1, 1. I think it's wrong.
 # (pin_num1, pin_num2)
-MOTOR_COAST_BITS = (1, 1)  # Z, Z
-MOTOR_REVERSE_BITS = (0, 1)  # L, H
-MOTOR_FORWARD_BITS = (1, 0)  # H, L
-MOTOR_BRAKE_BITS = (0, 0)  # L, L
+MOTOR_DC_COAST_BITS = (1, 1)  # Z, Z
+MOTOR_DC_REVERSE_BITS = (0, 1)  # L, H
+MOTOR_DC_FORWARD_BITS = (1, 0)  # H, L
+MOTOR_DC_BRAKE_BITS = (0, 0)  # L, L
 
 # Plus boards
-RELAY, MOTOR, DIGITAL = range(3)
+# Motor board IC datasheet: http://www.ti.com/lit/ds/symlink/drv8835.pdf
+RELAY, MOTOR_DC, MOTOR_STEPPER, DIGITAL = range(4)
 
 
 class NoPiFaceRelayPlusDetectedError(Exception):
     pass
 
 
-class Motor(object):
+class MotorDC(object):
     """A motor driver attached to a PiFace Relay Plus. Uses DRV8835."""
     def __init__(self, pin1, pin2):
         self.pin1 = pin1
@@ -30,23 +32,68 @@ class Motor(object):
 
     def coast(self):
         """Sets the motor so that it is coasting."""
-        self.pin1.value = MOTOR_COAST_BITS[0]
-        self.pin2.value = MOTOR_COAST_BITS[1]
+        self.pin1.value = MOTOR_DC_COAST_BITS[0]
+        self.pin2.value = MOTOR_DC_COAST_BITS[1]
 
     def reverse(self):
         """Sets the motor so that it is moving in reverse."""
-        self.pin1.value = MOTOR_REVERSE_BITS[0]
-        self.pin2.value = MOTOR_REVERSE_BITS[1]
+        self.pin1.value = MOTOR_DC_REVERSE_BITS[0]
+        self.pin2.value = MOTOR_DC_REVERSE_BITS[1]
 
     def forward(self):
         """Sets the motor so that it is moving forward."""
-        self.pin1.value = MOTOR_FORWARD_BITS[0]
-        self.pin2.value = MOTOR_FORWARD_BITS[1]
+        self.pin1.value = MOTOR_DC_FORWARD_BITS[0]
+        self.pin2.value = MOTOR_DC_FORWARD_BITS[1]
 
     def brake(self):
         """Stop the motor."""
-        self.pin1.value = MOTOR_BRAKE_BITS[0]
-        self.pin2.value = MOTOR_BRAKE_BITS[1]
+        self.pin1.value = MOTOR_DC_BRAKE_BITS[0]
+        self.pin2.value = MOTOR_DC_BRAKE_BITS[1]
+
+
+class MotorStepper(object):
+    """A stepper motor driver attached to a PiFace Relay Plus. Uses DRV8835."""
+
+    step_states = (0xa, 0x2, 0x6, 0x4, 0x5, 0x1, 0x9, 0x8)
+
+    def __init__(self, index, chip):
+        self.chip = chip
+        if index == 0:
+            self.set_stepper = self._set_stepper0
+        else:
+            self.set_stepper = self._set_stepper1
+
+    def _set_stepper0(self, value):
+        """GPIOB lower nibble, polarity reversed."""
+        gpiob = self.chip.gpiob.value & 0xf0
+        self.chip.gpiob.value = gpiob | ((value & 0xf) ^ 0xf)
+
+    def _set_stepper1(self, value):
+        """GPIOA upper nibble, normal polarity."""
+        gpioa = self.chip.gpioa.value & 0x0f
+        self.chip.gpioa.value = gpioa | ((value & 0xf) << 4)
+
+    def _send_steps(self, step_states, steps, step_delay):
+        for i in range(steps):
+            step_index = i % len(step_states)
+            self.set_stepper(step_states[step_index])
+            time.sleep(step_delay)
+
+    def coast(self):
+        """Sets the motor so that it is coasting."""
+        self.set_stepper(0x0)
+
+    def reverse(self, steps, step_delay):
+        """Sets the motor so that it is moving in reverse."""
+        self._send_steps(reversed(self.step_states), steps, step_delay)
+
+    def forward(self, steps, step_delay):
+        """Sets the motor so that it is moving forward."""
+        self._send_steps(self.step_states, steps, step_delay)
+
+    def brake(self):
+        """Stop the motor."""
+        self.set_stepper(0xf)
 
 
 class PiFaceRelayPlus(pifacecommon.mcp23s17.MCP23S17,
@@ -80,6 +127,8 @@ class PiFaceRelayPlus(pifacecommon.mcp23s17.MCP23S17,
                                                           pcmcp.GPIOB,
                                                           self)
 
+        self.relay_port = pcmcp.MCP23S17Register(pcmcp.GPIOA, self)
+
         # Relays are always lower nibble of GPIOA, order is reversed
         self.relays = list(reversed([pcmcp.MCP23S17RegisterBit(i,
                                                                pcmcp.GPIOA,
@@ -91,17 +140,22 @@ class PiFaceRelayPlus(pifacecommon.mcp23s17.MCP23S17,
             self.relays.extend([pcmcp.MCP23S17RegisterBit(i, pcmcp.GPIOA, self)
                                 for i in range(4, 8)])
 
-        elif plus_board == MOTOR:
+        elif plus_board == MOTOR_DC:
             self.motors = [
-                Motor(pin1=pcmcp.MCP23S17RegisterBitNeg(3, pcmcp.GPIOB, self),
-                      pin2=pcmcp.MCP23S17RegisterBitNeg(2, pcmcp.GPIOB, self)),
-                Motor(pin1=pcmcp.MCP23S17RegisterBitNeg(1, pcmcp.GPIOB, self),
-                      pin2=pcmcp.MCP23S17RegisterBitNeg(0, pcmcp.GPIOB, self)),
-                Motor(pin1=pcmcp.MCP23S17RegisterBit(4, pcmcp.GPIOA, self),
-                      pin2=pcmcp.MCP23S17RegisterBit(5, pcmcp.GPIOA, self)),
-                Motor(pin1=pcmcp.MCP23S17RegisterBit(6, pcmcp.GPIOA, self),
-                      pin2=pcmcp.MCP23S17RegisterBit(7, pcmcp.GPIOA, self)),
+                MotorDC(
+                    pin1=pcmcp.MCP23S17RegisterBitNeg(3, pcmcp.GPIOB, self),
+                    pin2=pcmcp.MCP23S17RegisterBitNeg(2, pcmcp.GPIOB, self)),
+                MotorDC(
+                    pin1=pcmcp.MCP23S17RegisterBitNeg(1, pcmcp.GPIOB, self),
+                    pin2=pcmcp.MCP23S17RegisterBitNeg(0, pcmcp.GPIOB, self)),
+                MotorDC(pin1=pcmcp.MCP23S17RegisterBit(4, pcmcp.GPIOA, self),
+                        pin2=pcmcp.MCP23S17RegisterBit(5, pcmcp.GPIOA, self)),
+                MotorDC(pin1=pcmcp.MCP23S17RegisterBit(6, pcmcp.GPIOA, self),
+                        pin2=pcmcp.MCP23S17RegisterBit(7, pcmcp.GPIOA, self)),
             ]
+
+        elif plus_board == MOTOR_STEPPER:
+            self.motors = [MotorStepper(i, self) for i in range(2)]
 
         elif plus_board == DIGITAL:
             pass
